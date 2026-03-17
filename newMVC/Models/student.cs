b21363be3eb3a58907148609d89d4dd3f42aab24
MoiBeak <tangdoandq@gmail.com>
2026-03-17 07:57:39 +0700

    BTH so7

--- newMVC/Models/student.cs
+++ newMVC/Models/student.cs
@@ -6,10 +6,18 @@ namespace newMVC.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Mã sinh viên không được để trống")]
+        [StringLength(10, ErrorMessage = "Mã sinh viên tối đa 10 ký tự")]
         public string StudentCode { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Tên sinh viên không được để trống")]
+        [StringLength(50, ErrorMessage = "Tên sinh viên tối đa 50 ký tự")]
         public string FullName { get; set; }
+
+        [Range(18, 30, ErrorMessage = "Tuổi phải từ 18 đến 30")]
+        public int Age { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
+        public string Email { get; set; }
     }
 }
